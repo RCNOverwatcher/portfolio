02988ad1bfbbc4775baa3aa0bc32a8e8085da81d
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-04-20 17:24:21 +0100

    update some stuff

--- components/ContactCode.jsx
+++ components/ContactCode.jsx
@@ -3,53 +3,28 @@ import styles from '../styles/ContactCode.module.css';
 const contactItems = [
   {
     social: 'website',
-    link: 'nitinranganath.me',
-    href: 'https://nitinranganath.me',
+    link: 'jacobwiltshire.dev',
+    href: 'https://jacobwiltshire.dev',
   },
   {
     social: 'email',
-    link: 'nitinranganath@gmail.com',
-    href: 'mailto:nitinranganath@gmail.com',
+    link: 'jacobjameswiltshire@protonmail.com',
+    href: 'mailto:jacobjameswiltshire@protonmail.com',
   },
   {
     social: 'github',
-    link: 'itsnitinr',
-    href: 'https://github.com/itsnitinr',
+    link: 'RCNOverwatcher',
+    href: 'https://github.com/RCNOverwatcher',
   },
   {
     social: 'linkedin',
-    link: 'nitinranganath',
-    href: 'https://www.linkedin.com/in/nitinranganath/',
+    link: 'jacobwiltshire',
+    href: 'https://www.linkedin.com/in/jacob-wiltshire-44b154244/',
   },
   {
-    social: 'twitter',
-    link: 'iamnitinr',
-    href: 'https://www.twitter.com/iamnitinr',
-  },
-  {
-    social: 'instagram',
-    link: 'iamnitinr',
-    href: 'https://www.instagram.com/iamnitinr',
-  },
-  {
-    social: 'polywork',
-    link: 'nitinranganath',
-    href: 'https://www.polywork.com/nitinranganath',
-  },
-  {
-    social: 'telegram',
-    link: 'iamnitinr',
-    href: 'https://t.me/iamnitinr',
-  },
-  {
-    social: 'codepen',
-    link: 'nitinranganath',
-    href: 'https://codepen.io/itsnitinr',
-  },
-  {
-    social: 'codesandbox',
-    link: 'itsnitinr',
-    href: 'https://codesandbox.io/u/itsnitinr',
+    social: 'discord',
+    link: 'RCN#0001',
+    href: 'https://large-type.com/#RCN%230001',
   },
 ];
 
@@ -68,7 +43,7 @@ const ContactCode = () => {
           ;
         </p>
       ))}
-      {contactItems.slice(8, contactItems.length).map((item, index) => (
+      {contactItems.slice(5, contactItems.length).map((item, index) => (
         <p className={styles.line} key={index}>
           &nbsp;&nbsp;{item.social}:{' '}
           <a href={item.href} target="_blank" rel="noopener">
