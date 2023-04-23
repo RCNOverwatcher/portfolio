cc620d812937fcb42b29a680e11fc6358e2cf6e4
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-04-23 12:30:20 +0100

    add analytics

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,33 @@
+import { Analytics } from '@vercel/analytics/react';
 import Link from 'next/link';
 import styles from '../styles/HomePage.module.css';
 import FadeIn from "react-fade-in";
 
 export default function HomePage() {
   return (
-    <FadeIn>
-      <div className={styles.container}>
-        <div className={styles.background}>
-          <h1>JACOB</h1>
-          <h1>WILTSHIRE</h1>
-        </div>
-        <div className={styles.foreground}>
-          <div className={styles.content}>
-            <h1 className={styles.name}>Jacob Wiltshire</h1>
-            <h6 className={styles.bio}>Computer Science Student</h6>
-            <Link href="/projects">
-              <button className={styles.button}>View Work</button>
-            </Link>
-            <Link href="/contact">
-              <button className={styles.outlined}>Contact Me</button>
-            </Link>
+      <>
+        <FadeIn>
+          <div className={styles.container}>
+            <div className={styles.background}>
+              <h1>JACOB</h1>
+              <h1>WILTSHIRE</h1>
+            </div>
+            <div className={styles.foreground}>
+              <div className={styles.content}>
+                <h1 className={styles.name}>Jacob Wiltshire</h1>
+                <h6 className={styles.bio}>Computer Science Student</h6>
+                <Link href="/projects">
+                  <button className={styles.button}>View Work</button>
+                </Link>
+                <Link href="/contact">
+                  <button className={styles.outlined}>Contact Me</button>
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </FadeIn>
+        </FadeIn>
+        <Analytics />
+      </>
   );
 }
 
