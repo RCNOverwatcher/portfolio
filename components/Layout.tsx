d34dd431d1d7f9abd79a62314059586c737eb6f6
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-04-28 09:03:39 +0100

    fix konami code

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,38 +1,69 @@
-import Titlebar from '../components/Titlebar';
-import Sidebar from '../components/Sidebar';
-import Explorer from '../components/Explorer';
-import Bottombar from '../components/Bottombar';
-import Tabsbar from './Tabsbar';
-import styles from '../styles/Layout.module.css';
+import Titlebar from "../components/Titlebar";
+import Sidebar from "../components/Sidebar";
+import Explorer from "../components/Explorer";
+import Bottombar from "../components/Bottombar";
+import Tabsbar from "./Tabsbar";
+import styles from "../styles/Layout.module.css";
 import useKonami from "react-use-konami";
-import {useState} from "react";
+import { useState } from "react";
 import Image from "next/image";
 
-
 const Layout = ({ children }) => {
-    const [isActive, setActive] = useState("false");
+  const [isActive, setActive] = useState(true);
 
-    useKonami(() => {
-        // @ts-ignore
-        setActive(isActive => !isActive);
-    });
-    useKonami(() => {
-        const kalebExposal = new Audio("https://res.cloudinary.com/dtqhs8nvm/video/upload/v1682450542/kaleb_a3zfyp.mp3");
-        kalebExposal.play();
-    }, {
-        code: ['k', 'a', 'l', 'e', 'b'],
-    });
+  useKonami(
+    () => {
+      const kalebExposal = new Audio(
+        "https://res.cloudinary.com/dtqhs8nvm/video/upload/v1682450542/kaleb_a3zfyp.mp3"
+      );
+      kalebExposal.play();
+    },
+    {
+      code: ["k", "a", "l", "e", "b"],
+    }
+  );
+
+    useKonami(
+        () => {
+            // @ts-ignore
+            setActive((isActive) => !isActive);
+        },
+        {
+            code: [
+                "ArrowUp",
+                "ArrowUp",
+                "ArrowDown",
+                "ArrowDown",
+                "ArrowLeft",
+                "ArrowRight",
+                "ArrowLeft",
+                "ArrowRight",
+                "b",
+                "a",
+                "Enter",
+            ],
+        }
+    );
+
   return (
     <>
       <Titlebar />
       <div className={styles.main}>
         <Sidebar />
         <Explorer />
-        <div style={{ width: '100%' }}>
+        <div style={{ width: "100%" }}>
           <Tabsbar />
           <main className={styles.content}>{children}</main>
         </div>
-          <Image className={isActive ? "hidekys" : null} src={"https://res.cloudinary.com/dtqhs8nvm/image/upload/v1682447998/kys_kvq9wm.gif"} alt={"hehe"} width={1000} height={1000}/>
+        <Image
+          className={isActive ? "hidekys" : null}
+          src={
+            "https://res.cloudinary.com/dtqhs8nvm/image/upload/v1682447998/kys_kvq9wm.gif"
+          }
+          alt={"hehe"}
+          width={1000}
+          height={1000}
+        />
       </div>
       <Bottombar />
     </>
