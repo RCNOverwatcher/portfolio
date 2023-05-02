b2fd304254262cc54e4cdaa754a4035841114028
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-05-02 13:59:20 +0100

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: "https://jacobwiltshire.com",
+  generateRobotsTxt: true,
+};
--- next.config.js
+++ next.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  siteUrl: "https://jacobwiltshire.com",
   images: {
     domains: [
-      'res.cloudinary.com',
-      'avatars.githubusercontent.com',
-      'imgur.com',
-      'i.imgur.com',
-      'media.tenor.com'
-    ]
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "imgur.com",
+      "i.imgur.com",
+      "media.tenor.com",
+    ],
   },
   async rewrites() {
     return [
       {
-        source: '/about',
-        destination: '/',
+        source: "/about",
+        destination: "/",
       },
       {
-        source: '/contact',
-        destination: '/',
+        source: "/contact",
+        destination: "/",
       },
       {
-      source: '/personal',
-      destination: '/',
+        source: "/personal",
+        destination: "/",
       },
       {
-        source: '/github',
-        destination: '/',
+        source: "/github",
+        destination: "/",
       },
       {
-        source: '/settings',
-        destination: '/',
-      }
-    ]
+        source: "/settings",
+        destination: "/",
+      },
+    ];
   },
 };
