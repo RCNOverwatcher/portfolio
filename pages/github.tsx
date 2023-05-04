d2364b99493109e816ad07dda119938f9b4c2b0e
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-05-04 21:14:30 +0100

    fix eslint

--- pages/github.tsx
+++ pages/github.tsx
@@ -66,6 +66,9 @@ export async function getStaticProps() {
     },
   );
   let repos = await repoRes.json();
+  repos = repos
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .slice(0, 8);
 
   return {
     props: { title: 'GitHub', repos, user },
